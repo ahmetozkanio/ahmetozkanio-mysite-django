12bf434a6a6a1b20ade209662c63ae52775bb43f
ahmetozkanio <bledwarrior@gmail.com>
2022-10-12 23:19:16 +0300

    install pillow and user, project model update

--- projects/models.py
+++ projects/models.py
@@ -3,9 +3,14 @@ from django.db import models
 # Create your models here.
 
 class ProjectModel(models.Model):
-    project_name = models.CharField()
+    #id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length = 128, unique = True)
+    description = models.TextField(blank = True,null = True)
+    date = models.DateTimeField()
+    image = models.ImageField(upload_to="projects/%Y/%m/%d/",default = "projects/default.png")
+    avaliable = models.BooleanField(default =True)
     #project_
 
 class ProjectImageModel(models.Model):
-    project = models.ForeignKey(ProjectModel)
-    project_images = models.ImageField()
+    project = models.ForeignKey(ProjectModel, null=True, on_delete=models.CASCADE)
+    project_images =  models.ImageField(upload_to="projects/%Y/%m/%d/",default = "projects/default.png")
--- user/models.py
+++ user/models.py
@@ -3,14 +3,12 @@ from django.db import models
 
 # Create your models here.
 class UserModel(models.Model):
-    
-    firs_name = models.CharField()
-    last_name = models.CharField()
-    title =  models.CharField()
+    firs_name = models.CharField(max_length = 64)
+    last_name = models.CharField(max_length = 64)
+    title = models.CharField(max_length = 128)
     image  = models.ImageField(upload_to="user")
     description = models.TextField(blank=True)
     birthday = models.DateTimeField(auto_now=True)
-    accounts = models.CharField()
     avaliable = models.BooleanField(default =True)
 
     def __str__(self):
@@ -18,6 +16,6 @@ class UserModel(models.Model):
 
 class Accounts(models.Model):
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='user_accounts')
-    account_name = models.CharField()
+    account_name = models.CharField(max_length = 64, unique = True)
     account_image = models.ImageField(upload_to="account")
     account_url = models.URLField(blank=True,null=True)
