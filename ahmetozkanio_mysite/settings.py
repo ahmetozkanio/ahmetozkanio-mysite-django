6556636dfdd562d3dac59e95f944952d74b830e9
ahmetozkanio <bledwarrior@gmail.com>
2022-10-12 23:05:15 +0300

    setting.py INSTALLED_APPS Configure

--- ahmetozkanio_mysite/settings.py
+++ ahmetozkanio_mysite/settings.py
@@ -32,16 +32,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    
-    'projects'
-    'user'
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'projects'
+    'user'
+
 ]
 
 MIDDLEWARE = [
