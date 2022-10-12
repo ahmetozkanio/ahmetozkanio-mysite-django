222a381f3ee1ef4c0881ba1758d4b7a42c59c8c0
ahmetozkanio <bledwarrior@gmail.com>
2022-10-13 00:58:42 +0300

    requirements update

--- ahmetozkanio_mysite/settings.py
+++ ahmetozkanio_mysite/settings.py
@@ -39,11 +39,22 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
+    'api',
     'projects',
     'user',
     'certificates',
 
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- certificates/admin.py
+++ certificates/admin.py
@@ -4,3 +4,5 @@ from certificates.models import CertificatesModel
 
 # Register your models here.
 admin.site.register(CertificatesModel)
+class CertificatesAdmin(admin.ModelAdmin):
+    list_display = ('name','company_name','date','avaliable')
